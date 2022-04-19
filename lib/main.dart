952766369898024923d850461b7e9a7aa6b9becd
darlincode <nwannachukwuemekad@gmail.com>
2022-04-19 19:59:01 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import 'page/fingerprint_page.dart';
-
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight
-    
-  ]);
-
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  static const String title = 'Local Auth';
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: true,
-        title: title,
-        theme: ThemeData(primarySwatch: Colors.purple),
-        home: FingerprintPage(),
-      );
-}
+import 'package:flutter/material.dart' ;
+import 'package:flutter/services.dart' ;
+import 'page/fingerprint_page.dart' ;
+ 
+ Future main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await SystemChrome.setPreferredOrientations([
+     DeviceOrientation.portraitUp,
+     DeviceOrientation.portraitDown,
+     DeviceOrientation.landscapeLeft,
+     DeviceOrientation.landscapeRight
+  ]); 
+ 
+   runApp(MyApp());
+} 
+ 
+ class MyApp extends StatelessWidget {
+   static const String title = 'Local Auth';
+   @override
+   Widget build(BuildContext context) => MaterialApp(
+         debugShowCheckedModeBanner: true,
+         title: title,
+         theme: ThemeData(primarySwatch: Colors.purple),
+         home: FingerprintPage(),
+      ); 
+} 
