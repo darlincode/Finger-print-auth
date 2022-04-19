2b65ff9f24b0b8e4423ae9c65ee5ad4cc3f23b7d
darlincode <nwannachukwuemekad@gmail.com>
2022-04-19 19:51:09 +0100

    new

--- lib/api/local_auth_api.dart
+++ lib/api/local_auth_api.dart
@@ -1,42 +1,42 @@
-import 'package:flutter/services.dart';
-import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart' ;
+import 'package:local_auth/local_auth.dart' ;
 
-class LocalAuthApi {
-  static final _auth = LocalAuthentication();
+class LocalAuthApi  {
+  static final _auth =  LocalAuthentication();
 
-  static Future<bool> hasBiometrics() async {
+  static Future<bool>  hasBiometrics() async {
     try {
-      return await _auth.canCheckBiometrics;
+      return await  _auth.canCheckBiometrics;
       // ignore: unused_catch_clause
-    } on PlatformException catch (e) {
-      return false;
-    }
-  }
+    } on  PlatformException catch (e) {
+      return  false;
+     }
+   }
 
-  static Future<List<BiometricType>> getBiometrics() async {
-    try {
-      return await _auth.getAvailableBiometrics();
-      // ignore: unused_catch_clause
-    } on PlatformException catch (e) {
-      return <BiometricType>[];
-    }
-  }
+   static Future<List<BiometricType>> getBiometrics() async {
+     try {
+       return await _auth.getAvailableBiometrics();
+       // ignore: unused_catch_clause
+     } on PlatformException catch (e) {
+       return <BiometricType>[];
+     }
+   }
 
-  static Future<bool> authenticate() async {
-    final isAvailable = await hasBiometrics();
-    if (!isAvailable) return false;
-    else(!isAvailable); return true;
+   static Future<bool> authenticate() async {
+     final isAvailable = await hasBiometrics();
+     if (!isAvailable) return false;
+     else(!isAvailable); return true;
 
-    // ignore: dead_code
-    try {
-      return await _auth.authenticateWithBiometrics(
-        localizedReason: 'Scan Fingerprint to Authenticate',
-        useErrorDialogs: true,
-        stickyAuth: true,
-      );
-      // ignore: unused_catch_clause
-    } on PlatformException catch (e) {
-      return false;
-    }
-  }
-}
+     // ignore: dead_code
+     try {
+       return await _auth.authenticateWithBiometrics(
+         localizedReason:'Scan Fingerprint to Authenticate',
+         useErrorDialogs: true,
+          stickyAuth: true,
+        );
+       // ignore: unused_catch_clause
+     } on PlatformException catch (e) {
+       return false;
+     }
+   }
+ }
