936c6ee5f9a9f27074999f882b11579a1afce262
darlincode <nwannachukwuemekad@gmail.com>
2022-04-19 19:44:06 +0100

    new

--- lib/api/local_auth_api.dart
+++ lib/api/local_auth_api.dart
@@ -7,7 +7,7 @@ class LocalAuthApi {
   static Future<bool> hasBiometrics() async {
     try {
       return await _auth.canCheckBiometrics;
-    // ignore: unused_catch_clause
+      // ignore: unused_catch_clause
     } on PlatformException catch (e) {
       return false;
     }
@@ -16,7 +16,7 @@ class LocalAuthApi {
   static Future<List<BiometricType>> getBiometrics() async {
     try {
       return await _auth.getAvailableBiometrics();
-    // ignore: unused_catch_clause
+      // ignore: unused_catch_clause
     } on PlatformException catch (e) {
       return <BiometricType>[];
     }
@@ -25,14 +25,16 @@ class LocalAuthApi {
   static Future<bool> authenticate() async {
     final isAvailable = await hasBiometrics();
     if (!isAvailable) return false;
+    else(!isAvailable); return true;
 
+    // ignore: dead_code
     try {
       return await _auth.authenticateWithBiometrics(
-        localizedReason:'Scan Fingerprint to Authenticate',
+        localizedReason: 'Scan Fingerprint to Authenticate',
         useErrorDialogs: true,
         stickyAuth: true,
       );
-    // ignore: unused_catch_clause
+      // ignore: unused_catch_clause
     } on PlatformException catch (e) {
       return false;
     }
